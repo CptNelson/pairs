ceb43f088d1e87cc314fbf68202c23e5a91f4725
CptNelson <toni.lappalainen@gmail.com>
2018-09-24 14:46:05 +0300

    all cards & backsides

--- src/pairs.js
+++ src/pairs.js
@@ -2,9 +2,9 @@
 let gameScene = new Phaser.Scene('Game');
 
 let config = {
-    type: Phaser.Auto, //webGL or canvas
-    width: 800,
-    height: 640,
+    type: Phaser.Auto, //Let the browser choose if using webGL or canvas
+    width: 1090,
+    height: 650,
     scene: gameScene
 };
 
@@ -12,107 +12,80 @@ let config = {
 let game = new Phaser.Game(config);
 
 gameScene.preload = function() {
-    this.load.image('card00', 'assets/tarot/00_fool.png');
-    this.load.image('card01', 'assets/tarot/01_magician.png');
-    this.load.image('card02', 'assets/tarot/02_high_priestess.png');
-    this.load.image('card03', 'assets/tarot/03_empress.png');
-    this.load.image('card04', 'assets/tarot/04_emperor.png');
-    this.load.image('card05', 'assets/tarot/05_pope.png');
+    this.load.multiatlas('tarots','assets/tarots.json', 'assets');
+    this.load.image('back', 'assets/back.png');
 };
 
-gameScene.create = function() {
 
-    this.shuffledCards = shuffle(this.textures.getTextureKeys());
+
+gameScene.create = function() {
+    
+    this.cardNamesA = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+                    '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                    '20', '21']
+    this.cardNamesB = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+                    '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                    '20', '21']
 
     this.tarotsA = [];
     this.tarotsB = [];
 
-    let dimensions = {
-        X: [10, 120, 230, 340, 450, 560],
+    this.dimensions = {
+        X: [10, 100, 190, 280, 370, 460, 550, 640,730,820,910,1000],
         Y: 10,
-        getX: function() { return this.X; },
-        getY: function() { return this.Y; },
-        setX: function(value) { this.X = value; },
-        setY: function(value) { this.Y = value; }
     }
-    
-/*     let x = [10, 120, 230, 340, 450, 560];
-    let y = 10; */
+    this.columnCount = 0;
 
-    let columnCount = {
-        set count(i) {
-            this.count(i)
-        },
-        count: 0
-    }
+    gameScene.deal();
 
-    for (let c = 0; c < this.shuffledCards.length; c++ ) {
-
-        let tarotsAX = getRandomInt(dimensions.X.length);
-        console.log("random number A: ", tarotsAX)
-        this.tarotsA[c] = this.add.sprite(dimensions.X[tarotsAX],dimensions.Y, this.shuffledCards[c])
-        //console.log("A X = ", x[tarotsAX])
-        //console.log("tarot A: ", this.tarotsA[c].x)
-        dimensions.X.splice(tarotsAX,1);
-        //console.log("x after A: ", x, " ", y)
-
-        let tarotsBX = getRandomInt(dimensions.X.length);
-        //console.log("random number B: ", tarotsBX)
-        this.tarotsB[c] = this.add.sprite(dimensions.X[tarotsBX],dimensions.Y, this.shuffledCards[c])
-        //console.log("B X = ", x[tarotsBX])
-        //console.log("tarot B: ", this.tarotsB[c].x)
-        dimensions.X.splice(tarotsBX,1);
-        //console.log("x after B: ",x, " ", y)
-
-        columnCounter(columnCount,dimensions);
-    }
-
-    for (c = 0; c < this.tarotsA.length; c++) {
-        //console.log(this.tarotsB[c]);
-        this.tarotsA[c].setOrigin(0,0);
-        this.tarotsA[c].setScale(0.5);
-        this.tarotsB[c].setOrigin(0,0);
-        this.tarotsB[c].setScale(0.5);
-    }
 }
 
-//Set scale and origin for images
+//Deal the cards randomly at table, keep their order in two packs for easy compare.
+gameScene.deal = function() {
+    for (let c = 0; c < 22; c++ ) {
+        let tarotsAX = getRandomInt(this.dimensions.X.length);
+        let randomCardA = getRandomInt(this.cardNamesA.length)
+        this.tarotsA[c] = this.add.sprite(this.dimensions.X[tarotsAX],this.dimensions.Y, 'tarots', this.cardNamesA[randomCardA])
+        this.back = this.add.sprite(this.dimensions.X[tarotsAX], this.dimensions.Y, 'back');
+        this.dimensions.X.splice(tarotsAX,1);
+        this.cardNamesA.splice(randomCardA,1);
+        this.back.setOrigin(0,0);
+        this.back.setScale(0.4);
+        
+        let tarotsBX = getRandomInt(this.dimensions.X.length);  
+        let randomCardB = getRandomInt(this.cardNamesB.length);
+        this.tarotsB[c] = this.add.sprite(this.dimensions.X[tarotsBX],this.dimensions.Y, 'tarots', this.cardNamesB[randomCardB])
+        this.back = this.add.sprite(this.dimensions.X[tarotsBX], this.dimensions.Y, 'back');
+        this.dimensions.X.splice(tarotsBX,1);
+        this.cardNamesB.splice(randomCardB,1);
+        this.back.setOrigin(0,0);
+        this.back.setScale(0.4);
+
+        gameScene.columnCounter();
+    }
+}
 
 
 //checks if there is enough columns to start a new row. 
 //If so, Y is changed to higher number, and X is initialized back to starting array.
-function columnCounter(counter,dimensions) {
-    counter.count = counter.count + 1;
-    console.log("counter: ",counter.count)
-    if (counter.count > 2) {
-        //console.log("col")
-        dimensions.setY(210);
-        dimensions.setX([10, 120, 230, 340, 450, 560]);
-        counter.count = 0;    
+gameScene.columnCounter = function() {
+    this.columnCount++;
+    if (this.columnCount == 6) {
+        this.dimensions.Y = 170;
+        this.dimensions.X = [100, 190, 280, 370, 460, 550, 640,730,820,910];
+        console.log("x2: ",this.dimensions.X)  
+    } 
+    if (this.columnCount == 11) {
+        this.dimensions.Y = 330;
+        this.dimensions.X = [100, 190, 280, 370, 460, 550, 640,730,820,910];
+        console.log("x3: ",this.dimensions.X)  
     }
-    //console.log(dimensions.getY(), " ", dimensions.getX())
+    if (this.columnCount == 16) {
+        this.dimensions.Y = 490;
+        this.dimensions.X = [10,100, 190, 280, 370, 460, 550, 640,730,820,910,1000];
+        console.log("x3: ",this.dimensions.X)  
+    }
 }
-
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
-}
-
-function shuffle(array) {
-    let counter = array.length;
-
-    // While there are elements in the array
-    while (counter > 0) {
-        // Pick a random index
-        let index = Math.floor(Math.random() * counter);
-
-        // Decrease counter by 1
-        counter--;
-
-        // And swap the last element with it
-        let temp = array[counter];
-        array[counter] = array[index];
-        array[index] = temp;
-    }
-
-    return array;
 }
